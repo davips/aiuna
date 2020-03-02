336032124a90c2337f9df84a7f6ce8ed9729fb8f
davips <dpsabc@gmail.com>
2020-03-02 15:21:48 -0300

    fixed toy

--- examples/toy.py
+++ examples/toy.py
@@ -4,7 +4,9 @@ from pjdata.data import Data
 from pjdata.dataset import Dataset
 
 # Testes            ############################
-dataset = Dataset('iris', 'Beautiful description.',
-                  X={'length': 'R', 'width': 'R'}, Y={'class': ['M', 'F']})
+dataset = Dataset('flowers', 'Beautiful description.')
+                  # X={'length': 'R', 'width': 'R'}, Y={'class': ['M', 'F']})
 data = Data(dataset, X=np.array([[1, 2, 3, 4], [5, 6, 7, 8]]),
             Y=np.array([[1, 2, 3, 4]]))
+
+print('OK')
