852958b38a9d622d7ff67469fb79a11c52de2ff4
davips <dpsabc@gmail.com>
2020-04-08 04:11:31 -0300

    minor improvement in toys

--- examples/toy.py
+++ examples/toy.py
@@ -1,12 +1,11 @@
 import numpy as np
 
 from pjdata.data import Data
-from pjdata.dataset import Dataset
 
 # Testes            ############################
-dataset = Dataset('flowers', 'Beautiful description.')
-                  # X={'length': 'R', 'width': 'R'}, Y={'class': ['M', 'F']})
-data = Data(dataset, X=np.array([[1, 2, 3, 4], [5, 6, 7, 8]]),
-            Y=np.array([[1, 2, 3, 4]]))
+data = Data(X=np.array([[1, 2, 3, 4], [5, 6, 7, 8]]),
+            Y=np.array([[1, 2, 3, 4]]),
+            Xd=['length', 'width'], Yd=['class'],
+            name='flowers', desc='Beautiful description.')
 
 print('OK')
