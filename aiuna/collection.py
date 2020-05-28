396241c827fd78dec7f20af1908c7fd2e9562a7d
davips <dpsabc@gmail.com>
2020-05-28 04:23:06 -0300

    Some IFs in Collection debug.

--- aiuna/collection.py
+++ aiuna/collection.py
@@ -2,15 +2,12 @@ from functools import lru_cache
 
 
 class Collection:
+    """
 
-    @property
-    def allfrozen(self):
-        raise Exception('não sabemos se será preciso!')
+    Evidently, a generator cannot be shared between Collection objects!
+    """
 
-    def _uuid_impl(self):
-        raise Exception('não sabemos se será preciso!')
-
-    def __init__(self, generator, finalizer, finite=True, debug_info=""):
+    def __init__(self, generator, finalizer, finite=True, debug_info=None):
         self.generator = generator
         self.finalizer = finalizer
         self.finite = finite
@@ -23,22 +20,33 @@ class Collection:
 
     def __next__(self):
         try:
-            print(self.debug_info, 'asks for next data...')
+            if self.debug_info:
+                print(self.debug_info, 'asks for next data...')
             data = next(self.generator)
-            print('...and', self.debug_info, 'got', type(data))
+            if self.debug_info:
+                print('...and', self.debug_info, 'got', type(data))
             if type(data) == tuple:
                 data, *self._last_args = data
             return data
         except StopIteration as e:
-            print('...no more data available for', self.debug_info)
+            if self.debug_info:
+                print('...no more data available for', self.debug_info)
             self._finished = True
             raise e from None
 
     @property
     @lru_cache()
     def data(self):
-        print(self.debug_info, 'asks for pendurado. Parametros:',
-              self._last_args)
+        if self.debug_info:
+            print(self.debug_info, 'asks for pendurado. Parametros:',
+                  self._last_args)
         if self.finite and not self._finished:
             raise Exception('Data object not ready!')
         return self.finalizer(*self._last_args)
+
+    # @property
+    # def allfrozen(self):
+    #     raise Exception('não sabemos se será preciso!')
+    #
+    # def _uuid_impl(self):
+    #     raise Exception('não sabemos se será preciso!')
