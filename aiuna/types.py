8aded450983a705409801f3422b8f6a488e6beab
davips <dpsabc@gmail.com>
2020-06-06 05:13:16 -0300

    Establish a proper file for types

---
+++ aiuna/types.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from typing import Union, Tuple, List, Literal, Callable, Type, Dict
+
+from numpy import ndarray  # type: ignore
+
+import pjdata.content.collection as c
+import pjdata.content.data as d
+import pjdata.content.specialdata as s
+
+Data = Union[Type[s.NoData], d.Data]
+DataTup = Tuple[Data, ...]  # HINT: Multi containing a Sink can produce heterogeneous tuples
+CollTup = Tuple[c.Collection, ...]
+DataOrTup = Union[Data, DataTup]
+CollOrTup = Union[c.Collection, CollTup]
+DataOrCollOrTup = Union[DataOrTup, CollOrTup]
+
+Field = Union[List[str], ndarray]  # For Data fields.
+Status = Union[str, bool, Literal['keep']]  # For frozen and hollow updates.
+Transformation = Callable[[Data], Data]  # Type of function transform(). Can return NoData because of Sink.
