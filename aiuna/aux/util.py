6ed76e5499d90e9c14d2de9c8f3ff9af75d69486
davips <dpsabc@gmail.com>
2020-06-28 08:12:39 -0300

    Improve itemgetter shortcut to accept class instantiation and function calling.

--- aiuna/aux/util.py
+++ aiuna/aux/util.py
@@ -1,5 +1,7 @@
 from typing import Union, Tuple, Any
 
+from compose import compose
+
 
 class _meta(type):
     def __getattr__(self, item):
@@ -7,8 +9,24 @@ class _meta(type):
 
 
 class _(metaclass=_meta):
+    """Shortcut for functional handling of iterables.
+ 
+    _.m = apply map and convert to list (useful for easy printing while debugging)
+    _ = item inside iterable (to provide as a function to map)
+
+    Example:
+        map(_[4], tuples)
+        map(_('My class name applied for all new instances'), classes)
+        _.m(_.id, users)
+    """
+
+    def __new__(cls, *args, **kwargs):
+        return lambda x: x(*args, **kwargs)
+
     def __class_getitem__(cls, item):
         return lambda x: x[item]
+
+    m = compose(list, map)
 
 
 def flatten(lst):
