031446f863cb5c06e8156f2cd1c2fb8ab0cf2b7a
davips <dpsabc@gmail.com>
2020-06-28 08:20:16 -0300

    Default values for iterables made explict through adopting str. Minor doc.

--- aiuna/transformer/pholder.py
+++ aiuna/transformer/pholder.py
@@ -13,6 +13,8 @@ from pjdata.aux.uuid import UUID
 
 
 class PHolder(Transformer):  # TODO: Find a better name? Skiper?
+    #TODO: lembrar por que o PHolder é necessário
+    #TODO: PHolder precisa ser posto apenas qnd usuário inibe step, ou mesmo quando frozen?
     """Placeholder for a component to appear in history but do nothing."""
 
     def __init__(self, component: WithSerialization):
