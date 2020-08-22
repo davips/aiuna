85c3e5c69a9a0b05064bd127c54eb0acfb0122c4
davips <dpsabc@gmail.com>
2020-08-22 12:35:53 -0300

    Data

--- examples/toy.py
+++ examples/toy.py
@@ -1,11 +1,18 @@
 import numpy as np
 
-from pjdata.data import Data
-
+from pjdata.aux.compression import pack
+from pjdata.aux.uuid import UUID
+from pjdata.content.data import Data
 # Testes            ############################
-data = Data(X=np.array([[1, 2, 3, 4], [5, 6, 7, 8]]),
-            Y=np.array([[1, 2, 3, 4]]),
-            Xd=['length', 'width'], Yd=['class'],
-            name='flowers', desc='Beautiful description.')
+from pjdata.history import History
 
-print('OK')
+matrices = {"X": np.array([[1, 2, 3, 4], [5, 6, 7, 8]]), "Y": np.array([[1, 2, 3, 4]]), "Xd": ['length', 'width'],
+            "Yd": ['class'], "Xt": ["real", "real", "real", "real"], "Yt": [1, 2, 3, 4]}
+uuids = {k: UUID(pack(v)) for k, v in matrices.items()}
+data = Data(
+    uuid=UUID(), uuids={"X": UUID(), "Y": UUID()},
+    failure=None, frozen=False, history=History([]), hollow=False, stream=None,
+    **matrices
+)
+
+print('OK', data)
