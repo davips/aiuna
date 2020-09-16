davips/pjdata_archived

Adapt first components to bullet proof architecture: cache, file, predictor

pjdata/transformer/model.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+from abc import abstractmethod
+from functools import lru_cache
 from typing import Any, TYPE_CHECKING, Union, Dict
 
+from pjdata.aux.util import Property
 from pjdata.mixin.serialization import withSerialization
 from pjdata.transformer.info import Info
 from pjdata.transformer.transformer import Transformer
@@ -11,19 +14,23 @@ if TYPE_CHECKING:
 
 
 class Model(Transformer):
-    def __init__(
-        self, component: withSerialization, func: t.Transformation, info: Union[Info, Dict[str, Any]], data: t.Data
-    ):
-        self._rawtransform = func
-        self.info = info if isinstance(info, Info) else Info(items=info)
-
+    def __init__(self, component: withSerialization, data: t.Data):
         # The multiplication order here cannot be otherwise, because it would mean "data output from enhancer".
-        # PCA is an example where enhUUID != modUUID; apesar de que ambos deveriam ser modUUID/ mas é impossível, pois enh não conhece o data futuro
+        # PCA is an example where enhUUID != modUUID; apesar de que ambos deveriam ser modUUID/ mas é impossível,
+        # pois enh não conhece o data futuro.
         self._uuid = component.cfuuid(data) * data.uuid
+        self.data = data
         super().__init__(component)
 
-    def rawtransform(self, content: t.Data) -> t.Result:
-        return self._rawtransform(content)
+    @Property
+    @lru_cache()
+    def info(self) -> Info:
+        info = self._info_impl(self.data)
+        return info if isinstance(info, Info) else Info(items=info)
+
+    @abstractmethod
+    def _info_impl(self, train):
+        pass
 
     def _uuid_impl(self):
         return self._uuid

davips/aiuna

aiuna/transformer/model.py
@@ -1,28 +1,31 @@
 from __future__ import annotations
 
+from abc import abstractmethod
+from functools import lru_cache
 from typing import Any, TYPE_CHECKING, Union, Dict
 
-from pjdata.mixin.serialization import WithSerialization
+from pjdata.mixin.serialization import withSerialization
 from pjdata.transformer.info import Info
 from pjdata.transformer.transformer import Transformer
 
-if TYPE_CHECKING:
-    import pjdata.types as t
-
 
 class Model(Transformer):
-    def __init__(
-        self, component: WithSerialization, func: t.Transformation, info: Union[Info, Dict[str, Any]], data: t.Data
-    ):
-        self._rawtransform = func
-        self.info = info if isinstance(info, Info) else Info(items=info)
-
-        # The multiplication order here helps to discover the data uuid: data.uuid = transformer.uuid / cfg_uuid
-        self._uuid = data.uuid * component.cfuuid
+    def __init__(self, component: withSerialization, data: t.Data):
+        # The multiplication order here cannot be otherwise, because it would mean "data output from enhancer".
+        # PCA is an example where enhUUID != modUUID; apesar de que ambos deveriam ser modUUID/ mas é impossível,
+        # pois enh não conhece o data futuro.
+        self._uuid = component.cfuuid(data) * data.uuid
+        self.data = data
         super().__init__(component)
 
-    def rawtransform(self, content: t.Data) -> t.Result:
-        return self._rawtransform(content)
+    @lru_cache()
+    def info(self) -> Info:
+        info = self._info_impl(self.data)
+        return info if isinstance(info, Info) else Info(items=info)
+
+    @abstractmethod
+    def _info_impl(self, train):
+        pass
 
     def _uuid_impl(self):
         return self._uuid