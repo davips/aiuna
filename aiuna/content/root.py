f300e9b48ba4d14d5b1f0066c4932102c6f90077
davips <dpsabc@gmail.com>
2020-10-19 03:33:55 -0300

    Simplify Root

--- aiuna/content/root.py
+++ aiuna/content/root.py
@@ -1,4 +1,6 @@
 from aiuna.content.data import Data
+from aiuna.history import History
+from cruipto.uuid import UUID
 
 
 class Root(Data):
@@ -6,18 +8,4 @@ class Root(Data):
         raise Exception("Root data is a singleton and cannot be instantiated!")
 
 
-class MetaRoot(type):
-    root = None
-
-    def __new__(mcs, *args, **kwargs):
-        if mcs.root is None:
-            from cruipto.uuid import UUID
-            from aiuna.history import History
-            mcs.root = Root(uuid=UUID(), uuids={}, history=History([]))
-        return mcs.root
-TODO adotar Root simples do StOverfl
-
-# Scala-like companion object for the instantiable class Root at the beginning of this file.
-# noinspection PyRedeclaration
-class Root(metaclass=MetaRoot):
-    """Singleton to feed Data processors."""
+Root = Root(UUID(), {}, History([]), UUID())
